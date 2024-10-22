6d255d75a498a40adc7b00bf0191acac62fb6c37
vially <viallymboma@users.noreply.github.com>
2024-10-22 21:22:50 +0100

    initiating the project

---
+++ src/components/Dashboard/E-commerce.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import ChartThree from "../Charts/ChartThree";
+import ChartTwo from "../Charts/ChartTwo";
+import ChatCard from "../Chat/ChatCard";
+import TableOne from "../Tables/TableOne";
+import MapOne from "../Maps/MapOne";
+import DataStatsOne from "@/components/DataStats/DataStatsOne";
+import ChartOne from "@/components/Charts/ChartOne";
+import { useTranslation } from "react-i18next";
+
+const ECommerce: React.FC = () => {
+  const { t } = useTranslation('common'); // Specify the namespace
+  return (
+    <>
+      <DataStatsOne />
+
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
+        {/* <div>
+          <h1>{t('welcome')}</h1>
+          <button>{t('logout')}</button>
+        </div> */}
+        <ChartOne />
+        <ChartTwo />
+        <ChartThree />
+        <MapOne />
+        <div className="col-span-12 xl:col-span-8">
+          <TableOne />
+        </div>
+        <ChatCard />
+      </div>
+    </>
+  );
+};
+
+export default ECommerce;
